99be5bec81ea9c2c442551a8d3dc14d0a61bf622
EzequielJes <88411304+EzequielJes@users.noreply.github.com>
2022-03-07 12:35:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -308,12 +308,12 @@ if (!isBotAdmin) return reply(mess.only.Badmin)
                 if (mek.message.extendedTextMessage != undefined){
                 mentioned = mek.message.extendedTextMessage.contextInfo.mentionedJid
 		if (!mentioned) return reply(`exampol xd: ${prefix + command} @participante... o etiqueta el mensaje de la persona a eliminar`)
-		await wa.FakeTokoForwarded(from, `Baaaiii...`, '')
+		await fs.FakeTokoForwarded(from, `Baaaiii...`, '')
 		if (mentionUser.length == 1)
 		client.groupRemove(from, mentionUser)
 		//client.groupRemove(from, mentioned)
 		} else {
-	        await wa.FakeTokoForwarded(from, `Baaaiii...`, '')
+	        await fs.FakeTokoForwarded(from, `Baaaiii...`, '')
 		client.groupRemove(from, mentioned)
 		}
 		break	
