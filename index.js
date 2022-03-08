b99e8147299787a1a90e55e32df3e20f02baf0db
EzequielJes <88411304+EzequielJes@users.noreply.github.com>
2022-03-08 20:42:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -295,6 +295,14 @@ ytmp3(play.all[0].url)
 break
 
 case 'dark':
+		if ((isMedia && !mek.message.videoMessage || isQuotedImage) && args.length == 0) {
+	var encmedia = isQuotedImage ? JSON.parse(JSON.stringify(mek).replace('quotedM', 'm')).message.extendedTextMessage.contextInfo : mek
+	var media = await client.downloadAndSaveMediaMessage(encmedia)
+	var ran = '666.webp'
+	await ffmpeg(`./${media}`)
+		.input(media)
+		.on('start', function (cmd) {
+	})
 		.on('end', function () {
 client.sendMessage(from, fs.readFileSync('./media/Nimue/oscuro.mp4'), sticker, {quoted: mek, contextInfo: {externalAdReply: fakeBot}})
 fs.unlinkSync(media)
