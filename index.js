c5cc93291447365ab7ba7e65c410eab66e818986
EzequielJes <88411304+EzequielJes@users.noreply.github.com>
2022-06-13 23:36:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -364,12 +364,12 @@ break
 if (!isGroup) return reply(mess.only.group)
 if (!isGroupAdmins) return reply(mess.only.admins)
 if (!isBotAdmin) return reply(mess.only.badmin)
-if (fs.mentionUser[0] === undefined) return reply('Mencione a un usuario')
-if (sender === fs.mentionUser[0]) return reply('No puede eliminar usted mismo')
-if (owner.includes(fs.mentionUser[0].split('@')[0])) return reply('No es posible eliminar a un owner del bot')
-if (groupAdmins.includes(fs.mentionUser[0])) return reply('No es posible eliminar a un administrador')
-client.groupParticipantsUpdate(mek, [fs.mentionUser[0]], 'remove')
-	.then(x => reply(`Ha sido eliminado @${fs.mentionUser[0].split('@')[0]} del grupo por @${senderNumber}`, {mentions: [fs.mentionUser[0], sender]}))
+if (fs.mentionedJid[0] === undefined) return reply('Mencione a un usuario')
+if (sender === fs.mentionedJid[0]) return reply('No puede eliminar usted mismo')
+if (owner.includes(fs.mentionedJid[0].split('@')[0])) return reply('No es posible eliminar a un owner del bot')
+if (groupAdmins.includes(fs.mentionedJid[0])) return reply('No es posible eliminar a un administrador')
+client.groupParticipantsUpdate(mek, [fs.mentionedJid[0]], 'remove')
+	.then(x => reply(`Ha sido eliminado @${fs.mentionedJid[0].split('@')[0]} del grupo por @${senderNumber}`, {mentions: [fs.mentionedJid[0], sender]}))
 	.catch(e => reply(e))
 break
 		
@@ -379,6 +379,7 @@ break
 if (!isGroup) return reply(mess.only.group)
 if (!isGroupAdmins) return reply(mess.only.admins)
 if (!isBotAdmin) return reply(mess.only.badmin)
+		client.groupParticipantsUpdate(mek, [fs.mentionedJid[0]], 'remove')
 		break
 		
 		
